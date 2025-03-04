88df0992c093ce049e535566bdcaa7ca5ef3b5e4
domM2365 <159438554+domM2365@users.noreply.github.com>
2025-03-04 10:48:26 +0000

    Code for Rx part of Command Processor

--- cmdProc.vhd
+++ cmdProc.vhd
@@ -1,137 +1,118 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.common_pack.all;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
 
 
 entity cmdProc is
 Port (
-	    --the following signals interact outside the entity cmdProc--
-	    --the titles of signals is what outside component they interact with--
-	    --note*** numWords, maxIndex and dataResults need to be changes to BCD array
-
-            --System--
+            -- System
             clk:    in std_logic;
             reset:  in std_logic;
             
-            -- Data Processor--
-            dataReady:   in std_logic;
+            -- DPCP Interface
+            dataReady:   in std_ulogic;
             byte:   in std_logic_vector (7 downto 0);
             maxIndex:   in std_logic_vector (11 downto 0);
             dataResults:   in std_logic_vector (55 downto 0);
-            seqDone:   in std_logic;
-            start:   out std_logic;
+            seqDone:   in std_ulogic;
+            start:   out std_ulogic;
             numWords:   out std_logic_vector (11 downto 0);
             
-            -- Transmitter-- 
-            dataOut:   out std_logic_vector (7 downto 0);
-            txNow:   out std_logic;
-            txDone:   out std_logic;
+            -- Transmitter 
+            dataout:   out std_logic_vector (7 downto 0);
+            txNow:   out std_ulogic;
+            txDone:   in std_ulogic;
             
-            --Reciever-- 
-            done:   out std_logic;
-            dataIn:   in std_logic_vector (7 downto 0);
-            valid:   in std_logic;
-            oe:   in std_logic;
-            fe:   in std_logic
+            -- Receiver 
+            rdone:   out std_ulogic;
+            rdatain:   in std_logic_vector (7 downto 0);
+            rvalid:   in std_ulogic;
+            roe:   in std_ulogic;
+            rfe:   in std_ulogic;
+            rinitiateNow: out std_ulogic;
+            rfinishInt: in std_ulogic
+            
 
  );
 end cmdProc;
 
+architecture Receiver of cmdProc is
+    type stateType is (IDLE, RECEIVING, RECEIVED, COMPLETE);
+    type logical_type is (TRUE, FALSE);
+    
+    signal currentState, nextState: stateType;
+    signal bit_count : integer range 0 to 8 := 0; --8 bit counter for received data
+    signal valid_reg : std_ulogic := '0'; -- internal signal to drive valid output
+    signal received_data : std_logic_vector(7 downto 0); -- stores received byte
+    
+begin  -- next state logic
+ receiverNextState: process(currentState, rdatain, bit_count)--add signals in here)
+begin
+-- defaults
+    nextState <= currentState;
+    valid_reg <= '0';
+    rdone <= '0';
+    
+    case currentState is
+        when IDLE =>
+            if rdatain /= "00000000" then --check if data is available
+                bit_count <= bit_count +1; -- include the bit that has already been sent
+                nextState <= RECEIVING;    
+            end if;
+            
+        when RECEIVING =>
+            if bit_count = 7 then --check if whole byte has been received
+                if rvalid = '1' then --valid signal is given by Rx
+                    nextState <= RECEIVED;
+                end if;
+            end if;   
+            
+        when RECEIVED =>
+            rdone <= '1'; --tells rx that data has been read and to clear register
+            rinitiateNow <= '1'; -- tells interface to do its thing
+            -- need to add if statements to determine oe and fe
+            if roe = '1' then
+                nextState <= IDLE;
+            end if;
+            if rfe = '1' then
+                nextState <= IDLE;
+            end if;
+            if rfinishInt = '1' then --received from Tx, internal finish signal
+                nextState <= COMPLETE;
+            end if;
+            
+        when others =>
+            nextState <= IDLE;
+     end case;
+ end process;
+ 
+ -- bit count process
+process(clk, rdatain)
+begin
+    if rising_edge(clk) then
+        if reset = '1' or currentState = IDLE then
+            bit_count <= 0;
+        elsif currentState = RECEIVING and rdatain /= "00000000" then -- detect bit input
+            bit_count <= bit_count + 1;
+        end if;
+     end if;
+ end process;
+    
+stateRegister: process(clk, reset)
+begin
+    if rising_edge(clk) then
+        if (reset = '1') then
+            currentState <= IDLE;
+        else
+            currentState <= nextState;
+        end if;
+    end if;
+   end process;
+    
+ -- assign outputs
+ dataout <= received_data;
 
-architecture Structure of cmdProc is
-	
-	component recieverInterface is
-		port( --the word beginning "r" denoted the reciever--
-			clk: in std_logic; --external related signal--
-			reset: in std_logic; --external related signal--
-			rdone: out std_logic; --external related signal--
-			rdataIn: in std_logic_vector (7 downto 0); --external related signal--
-			rvalid: in std_logic; --external related signal--
-			roe: in std_logic; --external related signal--
-			rfe: in std_logic; --external related signal--
-			rinitiateNow: out std_logic; --internal related signal--
-			rfinishInt: in std_logic --internal related signal--
-		);
-	end component;
+end Receiver;
 
-	component cmd2dataInterface is
-		port( --the word beginning "in" denoted the main interface--
-			clk: in std_logic; --external related signal--
-			reset: in std_logic; --external related signal--
-			instart: out std_logic; --external related signal--
-			innumWords: out std_logic_vector (11 downto 0); --external related signal--
-			indataReady: in std_logic; --external related signal--
-			inseqDone: in std_logic; --external related signal--
-			ininitiateNow: in std_logic; --internal related signal--
-			indataReadyint: out std_logic; --internal related signal--
-			inseqDoneint: out std_logic --internal related signal--
-		);	
-	end component;
-
-	component transmitterInterface is
-		port(  --the word beginning t denoted the Transmitter--
-			clk: in std_logic; --external related signal--
-			reset: in std_logic; --external related signal--
-			tdata: out std_logic_vector (7 downto 0); --external related signal--
-			ttxNow: out std_logic; --external related signal--
-			ttxDone: out std_logic; --external related signal--
-			tbyte: in std_logic_vector (7 downto 0); --external related signal--	
-			tmaxIndex: in std_logic_vector (11 downto 0); --external related signal--
-			tdataResults: in std_logic_vector (55 downto 0); --external related signal--
-			tdataReadyint: in std_logic; --internal related signal--
-			tseqDoneint: in std_logic; --internal related signal--
-			tfinishInt: out std_logic --internal related signal--
-		);
-	end component;
-
-	--internal signals--
-	signal initiateNow: std_logic := '0';
-	signal dataReadyint: std_logic := '0';
-	signal seqDoneint: std_logic := '0';
-	signal finishInt: std_logic := '0';
-
-	begin
-	
-		--reciever interface instantiation--
-		recInt: recieverInterface
-			port map(
-				clk => clk,
-				reset => reset,
-				rdone => done,
-				rdataIn => dataIn,
-				rvalid => valid,
-				roe => oe,
-				rfe =>fe,
-				rinitiateNow => initiateNow,
-				rfinishInt => finishInt
-			);
-
-		cdInt: cmd2dataInterface
-			port map(
-				clk => clk,
-				reset => reset,
-				instart => start,
-				innumWords => numWords,
-				indataReady => dataReady,
-				inseqDone => seqDone,
-				ininitiateNow => initiateNow,
-				indataReadyint => dataReadyint,
-				inseqDoneint => seqDoneint
-			);
-
-		tranInt: transmitterInterface 
-			port map(
-				clk => clk,
-				reset => reset,
-				tdata => dataOut,
-				ttxNow => txNow,
-				ttxDone => txDone,
-				tbyte => byte,
-				tmaxIndex => maxIndex,
-				tdataResults => dataResults,
-				tdataReadyint => dataReadyint,
-				tseqDoneint => seqDoneint,
-				tfinishInt => finishInt
-			);
-end Structure;
 
